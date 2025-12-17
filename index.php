51fa34ab928d18f3fc783e2eea16a292eb5293d1
Вадим <vadym.harasym.kb.2024@lpnu.ua>
2025-12-17 20:40:01 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,16 +3,15 @@ require_once __DIR__ . '/../auth.php';
 require_once __DIR__ . '/../partials/header.php';
 
 require_login();
-require_role(['admin']);
+require_role(['admin','realtor']);
 ?>
 
-
-<h2>Admin Panel</h2>
+<h2>Кабінет ріелтора</h2>
 
 <div class="card">
   <div class="actions">
-    <a class="btn" href="/property_agencie/admin/purchases.php">Заявки на покупку</a>
-    <a class="btn btn--ghost" href="<?php echo BASE_URL; ?>/sales_list.php">Заявки на продаж</a>
+    <a class="btn" href="<?php echo BASE_URL; ?>/sales_list.php">Заявки на продаж</a>
+    <a class="btn btn--ghost" href="<?php echo BASE_URL; ?>/sales_add.php">Додати продаж</a>
     <a class="btn btn--ghost" href="<?php echo BASE_URL; ?>/deal_create.php">Створити угоду</a>
   </div>
 </div>
